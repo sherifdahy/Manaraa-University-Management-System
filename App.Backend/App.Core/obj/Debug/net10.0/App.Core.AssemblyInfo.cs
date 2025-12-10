07f06e6e14d1cc8cc7dfb37a67a885704551d6ea
OmarZaky <omarzaky078@gmail.com>
2025-12-10 15:16:45 +0200

    nothing

--- App.Backend/App.Core/obj/Debug/net10.0/App.Core.AssemblyInfo.cs
+++ App.Backend/App.Core/obj/Debug/net10.0/App.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d945518d53ab4bd1dcf12cdc8031091f82e9d5c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0e562c602f4f4f742666fca4901f347a082d4f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
